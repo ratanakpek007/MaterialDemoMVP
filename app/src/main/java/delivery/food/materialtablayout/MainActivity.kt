b26baf9f3c36e0e2ea426ad56a33264333f4d269
Ratanak Pek <ratanakpek088@gmail.com>
2018-06-09 00:22:51 +0700

    init

---
+++ app/src/main/java/delivery/food/materialtablayout/MainActivity.kt
@@ -0,0 +1,58 @@
+package delivery.food.materialtablayout
+
+import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+         * 1
+         * */
+        /*  setSupportActionBar(toolbar)
+          toolbar.title="I am second"
+          //supportActionBar!!.title="Toolbar Demo"
+
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+              toolbar.elevation=10f
+          } else {
+
+          }*/
+
+
+        setSupportActionBar(toolbar)
+        toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.home -> Toast.makeText(this, "Home", Toast.LENGTH_LONG).show()
+            R.id.general -> Toast.makeText(this, "General", Toast.LENGTH_LONG).show()
+            R.id.pending -> Toast.makeText(this, "Pending", Toast.LENGTH_LONG).show()
+            R.id.logout -> Toast.makeText(this, "Logout", Toast.LENGTH_LONG).show()
+        }
+
+        return false
+    }
+}
